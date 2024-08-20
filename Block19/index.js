6f7dc3b285a55f2b04591db45387991b8f3fdd30
MickeyWeiss <167358315+MickeyWeiss@users.noreply.github.com>
2024-08-19 21:03:24 -0600

    Add files via upload

--- Block19/index.js
+++ Block19/index.js
@@ -1,54 +1,39 @@
+console.log('Hello')
+
+const names = ['Alice', 'Bob', 'Carol', 'Joe', 'Allison']
+const occupations = ['Writer', 'Teacher', 'Programmer', 'Engineer', 'Lawyer']
+
 const freelancers = [
-    {Name: "Alice", Occupation: "Writer", Price: "$30"},
-    {Name: "Bob", Occupation: "Teacher", Price: "$50"},
-    {Name: "Carol", Occupation: "Programmer", Price: "$70"},
-    {Name: "Joe", Occupation: "Engineer", Price: "$100"},
-    {Name: "Allison", Occupation: "Lawyer", Price: "$250"}
+    {name: "Alice", occupation: "Writer", price: "$30"},
+    {name: "Bob", occupation: "Teacher", price: "$50"},
+    {name: "Carol", occupation: "Programmer", price: "$70"},
+    {name: "Joe", occupation: "Engineer", price: "$100"},
+    {name: "Allison", occupation: "Lawyer", price: "$250"}
 ];
 
-function createTableRow(freelancer){
-    const row = document.createElement('tr')
+function renderFreelancers() {
+    const container = document.querySelector("#freelancers");
+    freelancers.forEach((freelancer) => {
+        container.innerHTML += `<div> Name: ${freelancer.name}, Occupation: ${freelancer.occupation}, Starting Price: ${freelancer.price}`
+    })
+}
+renderFreelancers()
 
-    const name = document.createElement('td')
-    name.textContent = freelancer.name
-    const occupation = document.createElement('td')
-    occupation.textContent = freelancer.occupation
-    const price = document.createElement('td')
-    price.textContent = freelancer.price
-
-    row.append(name)
-    row.append(occupation)
-    row.append(price)
-
-    return row
+function averagePrice() {
+    const totalPrice = freelancers.reduce((accumulator, currentValue) => accumulator + (currentValue.price), 0);
+    const avgPrice = totalPrice/freelancers.length;
+    document.querySelector('#avgPrice').textContent = `Average Freelancer Price: $${avgPrice}`
 
 }
+averagePrice()
 
-function setAveragePrice(index){
-   console.log(index)
+function randomFreelancer () {
+    const random = freelancers[Math.floor(Math.random() * freelancers.length)]
 
-   let sum = 0
-   for(let i = 0; i <= index; i++){
-    sum += freelancers[i].price
-   }
-   console.log("Average Price: ")
-   return sum / index
-
+    return {random}
 }
 
-let index = 0
-let internal = setInterval(() => {
-    let nextRow = createTableRow(freelancers[index])
-    index += 1
-
-    if(index === freelancers.length - 1){
-        clearInterval(internal)
-    }
-
-    const table = document.querySelector('table')
-    table.append(nextRow)
-
-    let avgPrice = setAveragePrice(index)
-    const priceSpan = document.querySelector('#price')
-    priceSpan.append('#price')
-}, 1000)
+// setInterval(() => {
+//     freelancers.push(randomFreelancer())
+//     renderFreelancers()
+// }, 1000)
